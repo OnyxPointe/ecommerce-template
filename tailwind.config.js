8f50717ec4b1a9b298c3f0321eb0e5679ffa1b87
NLaPointe21 <72793964+NLaPointe21@users.noreply.github.com>
2022-11-25 08:43:23 +0100

    Added MemberSpace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@ module.exports = {
   content: [
     './_includes/**/*.{html,md}',
     './_layouts/**/*.{html,md}',
-    './_*/*.{html,md}',
+    './_*/**/.{html,md}',
+    '!./_site/**/*.{html,md}',
     './*.{html,md}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'grey': '#7C7C7C',
+        'rich': '#101016',
+      },
+      fontFamily: {
+        featured: ['Poiret One', 'sans-serif']
+      },
+      height: {
+        'tall-50': '50vh',
+      }
+    },
   },
+  
   plugins: [],
 }
