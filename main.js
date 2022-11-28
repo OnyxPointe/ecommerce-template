70566e5012d4eb83e68d92fcee9411bc3803ad98
NLaPointe21 <72793964+NLaPointe21@users.noreply.github.com>
2022-11-28 06:28:22 +0100

    cc

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-console.log('something happened');
-// FAQ Dropdown
+// -----------FAQ DROPDOWN
 const buttons = document.querySelectorAll('.expand-button')
 const expandingSection = document.querySelectorAll('.expandable')
 
 function showMore(){
   let targetExpandableDiv = this.dataset.targetButton;
+
+  buttons.forEach(button => {
+    if (targetExpandableDiv == button.dataset.targetButton){
+      button.classList.toggle('active');
+    } else {
+      button.classList.remove('active');
+    }
+  })
+
   expandingSection.forEach(section => {
     if (targetExpandableDiv == section.dataset.targetMore){
       section.classList.toggle('hidden');
@@ -19,7 +27,7 @@ buttons.forEach(button => {
 })
 
 
-// Sticky Header
+// -----------STICKY HEADER
 const header = document.querySelector ('.sticky-header');
 
 const stickyNav = document.querySelector('.sticky-nav');
@@ -36,7 +44,7 @@ function stickyScroll(){
 console.log(scrollHeight);
   if (window.scrollY > scrollHeight){
     if(navToTop < -30){
-      stickyNav.style.top = '-80px';
+      stickyNav.style.top = '-120px';
       // stickyNav.classList.add('hide');
     } else {
       // stickyNav.classList.remove('hide');
@@ -52,19 +60,63 @@ console.log(scrollHeight);
 
 window.addEventListener('scroll', stickyScroll);
 
-
-// Slick slider
-$(document).ready(function(){
-  $('.slider-container').slick({
-    autoplay: true,
-    autoplaySpeed: 8000,
-    speed: 200,
-    arrows: false,
-    accessibility: true,
-    dots: true, 
-    fade: true,
-    infinite: true,
-    pauseOnHover: true,
-    pauseOnDotHover: true
-  });
+// -----------SLICK SLIDER
+$('.slider-container').slick({
+  autoplay: true,
+  autoplaySpeed: 6000,
+  speed: 200,
+  arrows: false,
+  accessibility: true,
+  dots: true, 
+  fade: true,
+  infinite: true,
+  pauseOnHover: true,
+  pauseOnDotHover: true
 });
+
+// -----------BEFORE/AFTER SLIDER
+$("#ba-slider").on("change", (e)=>{
+  const sliderPos = e.target.value;
+  // Update the width of the foreground image
+  $('.ba-foreground-img').css('width', `${sliderPos}%`)
+  // Update the position of the slider button
+  $('.ba-slider-button').css('left', `calc(${sliderPos}% - 18px)`)
+});
+
+// // -----------DARK MODE
+// // Step #1. Let's start by defining all the const we need. In this activity we need the body and our dark mode button 
+//     const body = document.querySelector('body');
+//     const darkModeButton = document.querySelector('#dark-btn');
+
+//     // Step #8. Now because we are playing with cookies we want to run some code when the page loads to see if our dark mode cookie is active. To do this we use a classic If statement.
+//     // this is checking what state the cookie is in on page load 
+//     if (document.cookie.includes('dark-mode=active')) {
+//         // Step #9. If was active then we want to add darkMode to the body's class list and we are done
+//         // if the cookie says darkMode active then add the class of darkMode to the body 
+//         body.classList.add('darkMode');
+//     }
+
+//     // Step #2. Just like our classic darkMode button lets start with the toggleDarkMode function
+//     // this is new function for flipping between light and dark mode
+//     function toggleDarkMode() {
+//         // this adds or removes darkMode from the body's class list 
+//         // Step #3. Lets toggle the body's darkMode class using   
+//         body.classList.toggle('darkMode');
+//         // this is checking the classlist again
+//         // Step #4. So that should all look similar to the button we did on the only course but now we get to the fun/scary part. First we are going to use an If statement to look at the body's classlist for darkMode
+//         if (body.classList.contains('darkMode')) {
+//             //if the body contains darkMode then we want to make a cookie that says its active 
+//             //if its set to darkMode then make a cookie saying its active
+//             document.cookie = "dark-mode=active";
+//         }
+//         //otherwise we want to make our cookie say that darkMode is deactivated
+//         else {
+//             // If its not then it is setting it to de-active
+//             document.cookie = "dark-mode=de-active";
+//         };
+//             // This is more troubleshooting code
+//             console.log('clicked on ' + document.cookie)
+//     };
+
+//     // Step #5. This is tying the darkMode button to the toggleDarkMode function
+//     darkModeButton.addEventListener('click', toggleDarkMode);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'dark-grey': '#494949',
         'grey': '#7C7C7C',
         'rich': '#101016',
+        'teal-blue': '#005A74',
       },
       fontFamily: {
         featured: ['Poiret One', 'sans-serif']
       },
       height: {
         'tall-50': '50vh',
+      },
+      spacing: {
+        '5p': '5%',
       }
     },
   },
